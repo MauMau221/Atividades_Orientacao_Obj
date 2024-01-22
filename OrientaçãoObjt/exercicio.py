7ce5fa861fa330cf64cac6e7eb1e880c33ecf645
MauMau221 <mauriciobgsp@gmail.com>
2024-01-22 00:09:02 -0300

    Correção no exercicio joquempo

--- OrientaçãoObjt/exercicio.py
+++ OrientaçãoObjt/exercicio.py
@@ -22,23 +22,23 @@ jogadordois.jogo = (random.choice(foo))
 
 
 if(jogadorum.jogo == 'Pedra') and (jogadordois.jogo == 'Tesoura'):
-    print('Jogador 1 ganhou.  ', 'O jogo do jogador doius foi', jogadordois.jogo)
+    print('Jogador 1 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 elif(jogadorum.jogo == 'Papel') and (jogadordois.jogo == 'Pedra'):
-    print('Jogador 1 ganhou.  ', 'O jogo do jogador doius foi', jogadordois.jogo)
+    print('Jogador 1 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 elif(jogadorum.jogo == 'Tesoura') and (jogadordois.jogo == 'Papel'):
-    print('Jogador 1 ganhou.  ', 'O jogo do jogador doius foi', jogadordois.jogo)
+    print('Jogador 1 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 
 elif(jogadordois.jogo == 'Pedra') and (jogadorum.jogo == 'Tesoura'):
-    print('Jogador 2 ganhou.  ', 'O jogo do jogador doius foi', jogadorum.jogo)
+    print('Jogador 2 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 elif(jogadordois.jogo == 'Papel') and (jogadorum.jogo == 'Pedra'):
-    print('Jogador 2 ganhou.  ', 'O jogo do jogador doius foi', jogadorum.jogo)
+    print('Jogador 2 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 elif(jogadordois.jogo == 'Tesoura') and (jogadorum.jogo == 'Papel'):
-    print('Jogador 2 ganhou.  ', 'O jogo do jogador doius foi', jogadorum.jogo)
+    print('Jogador 2 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 else:
     print('EMPATE', 'Jogador', jogadorum.nome, 'Meu jogo é: ', jogadorum.jogo,' E jogador ', jogadordois.nome, 'O jogo é : ', jogadordois.jogo)
