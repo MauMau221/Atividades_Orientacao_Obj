73e59f49831b200efe1f6a7e1d8a4841f9c2264b
MauMau221 <mauriciobgsp@gmail.com>
2024-01-22 00:13:18 -0300

    Correção no exercicio joquempo

--- OrientaçãoObjt/exercicio.py
+++ OrientaçãoObjt/exercicio.py
@@ -1,6 +1,7 @@
 class Jogador:
     def __init__(self, nome, jogo):
         self.nome = nome
+        self.jogo = jogo
 
     def darnome(self):
         self.nome = input('Informe o nome do jogador um: ')
@@ -20,7 +21,7 @@ foo = ['Pedra','Papel','Tesoura']
 jogadordois.jogo = (random.choice(foo))
 
 
-
+#Possibilidades onde o jogador um ganha 
 if(jogadorum.jogo == 'Pedra') and (jogadordois.jogo == 'Tesoura'):
     print('Jogador 1 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
@@ -31,6 +32,8 @@ elif(jogadorum.jogo == 'Tesoura') and (jogadordois.jogo == 'Papel'):
     print('Jogador 1 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
 
+
+#Possibilidades onde o jogador dois ganha 
 elif(jogadordois.jogo == 'Pedra') and (jogadorum.jogo == 'Tesoura'):
     print('Jogador 2 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
@@ -40,5 +43,7 @@ elif(jogadordois.jogo == 'Papel') and (jogadorum.jogo == 'Pedra'):
 elif(jogadordois.jogo == 'Tesoura') and (jogadorum.jogo == 'Papel'):
     print('Jogador 2 ganhou.  ', 'O jogo do jogador dois escolheu', jogadordois.jogo)
 
+
+#EMPATE
 else:
     print('EMPATE', 'Jogador', jogadorum.nome, 'Meu jogo é: ', jogadorum.jogo,' E jogador ', jogadordois.nome, 'O jogo é : ', jogadordois.jogo)
